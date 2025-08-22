1282a8ac894562468d6bf327df1946e02f30e00c
mhtariq366 <mhtariq366@gmail.com>
2025-08-22 21:52:17 +0500

    pf - 01 updated

--- programming fundamentals/01.cpp
+++ programming fundamentals/01.cpp
@@ -1,3 +1,16 @@
+/*
+C++ is used for various purposes. It is designed to help in both high level and low level tasks.
+It has good library support and helps in multiple tasks such as gaming development, general purpose tasks, 
+system programming, embedded systems etc.
+
+Below is the basic structure of C++ code.
+*/
+
+
+/*
+This is a header file. it contains two elements, preprocessor directive #include and library name which is
+written 'iostream'. There are differnt libraries used for different purposes. 
+*/
 #include <iostream>
 
 using namespace std;
