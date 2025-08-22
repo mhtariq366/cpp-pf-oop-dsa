8c761d335d4eb08bc771d45abedb27288b846387
mhtariq366 <mhtariq366@gmail.com>
2025-08-22 22:08:12 +0500

    pf - 01 updated

---
+++ programming fundamentals/01_basic_structure.cpp
@@ -0,0 +1,26 @@
+/*
+C++ is used for various purposes. It is designed to help in both high level and low level tasks.
+It has good library support and helps in multiple tasks such as gaming development, general purpose tasks, 
+system programming, embedded systems etc.
+
+Below is the basic structure of C++ code.
+*/
+
+
+/*
+This is a header file. it contains two elements, preprocessor directive #include and library name which is
+written 'iostream'. There are differnt libraries used for different purposes. We'll discuss as moving forward.
+*/
+#include <iostream>
+
+/*
+This is the main function. We'll write all pieces of code in this block, until we study the topic of functions itself.
+Code written outside the body of main function is not executable unless it is referenced in the main function's body.
+Main functions body is represented by the { } curly braces. Every thing inside these braces is part of main function's body.
+Main function can be written in two ways, either using 'int main' or 'void main'. If you're using int main return 0 statement needs
+to be written at the end of function. We'll discuss why its this way later on. For now just just focus on basic structure.
+*/
+int main(){
+
+    return 0;
+}
