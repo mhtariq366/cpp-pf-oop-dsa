dcfaef32f5709295d0438224674f7efe898117ae
mhtariq366 <mhtariq366@gmail.com>
2025-08-22 22:20:31 +0500

    pf - 02 comments added

---
+++ programming fundamentals/02_comments.cpp
@@ -0,0 +1,24 @@
+/*
+Before proceeding further. Lets talk about comments as they'll be used extensively here explaining different concepts.
+COmments are totally ignored by the compiter. These are used to explain and understand lines of code. Mostly by humans.
+
+There are two types of comments:
+1.  Single line comment
+2.  Multi line comment
+*/
+
+
+/*
+This is a multiline comment. You can write as many lines between these symbols
+This is a multiline comment. You can write as many lines between these symbols
+This is a multiline comment. You can write as many lines between these symbols
+This is a multiline comment. You can write as many lines between these symbols
+This is a multiline comment. You can write as many lines between these symbols
+This is a multiline comment. You can write as many lines between these symbols
+This is a multiline comment. You can write as many lines between these symbols
+*/
+
+
+//  This is single line comment. To write more than one line,
+//  You need to add double slash at start of every line. to avoid this, we use multi line comments.
+
